d4bec52974978bd89ec4e3a9ae17f704731a347f
RavanIsmayilov <rvanismayilov70@gmail.com>
2023-12-27 11:37:58 +0400

    ..

--- js/main.js
+++ js/main.js
@@ -4,19 +4,15 @@ var button = document.querySelector(".btn");
 
 const arr = [
     {
-        paragraf: "  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! Ravan quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! Ravan Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! Ravan Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read "
-    }
-]
-
-const arr2 = [
+        paragraf: "  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi int quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just rea  agni! just read  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdamagni! just read  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read Lorem, ips. Nam, magni! just read Lorem, ips agni! just read  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read Lorem, ips!  "
+    },
     {
-        paragraf: "  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read jkahsdkjas molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! Ravan Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read  molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! Ravan Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read "
-    }
-]
-
-const arr3 = [
+        paragraf2: "  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just rea sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just rea  "
+      
+    },
     {
-        paragraf: "  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read kslajdklajkldad molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! Ravan Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read  molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! Ravan Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read "
+        paragraf3: "  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi d sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just rea sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just read Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint quisquam commodi dolores beatae, aspernatur magni earum nihil neque placeat, molestias ullam! Cupiditate cumque vero tempora reprehenderit quibusdam. Nam, magni! just rea "
+       
     }
     
 ]
@@ -42,7 +38,7 @@ var tabContents = document.querySelector(".text-content");
 
 
 arr.forEach(element => {
-
+    
     var history = document.createElement('div');
 
     history.classList.add("history-text")
@@ -53,82 +49,121 @@ arr.forEach(element => {
 
     const paragraf = document.createElement('p'); 
 
+    const paragraf2 = document.createElement('p'); 
+
+    const paragraf3 = document.createElement('p');
+
+    paragraf3.textContent = element.paragraf3
+
+    paragraf2.textContent = element.paragraf2;
+
     paragraf.textContent = element.paragraf;
 
     history.appendChild(paragraf);
-})
+    history.appendChild(paragraf2);
+    history.appendChild(paragraf3);
 
 
 
-arr2.forEach(element => {
-
-    
-    var vision = document.createElement('div');
-    
-    vision.classList.add('vision-text');
-
-    vision.classList.add('hidden');
-
-    tabContents.appendChild(vision);
-
-
-    const paragraf2 = document.createElement('p'); 
-
-    paragraf2.textContent = element.paragraf;
-
-    vision.appendChild(paragraf2);
-
-})
-
-
-arr3.forEach(element => {
-
-    var goals = document.createElement('div');
-
-    goals.classList.add('goals-text');
-    
-    goals.classList.add('hidden');
-    
-    tabContents.appendChild(goals);
-
-
-    const paragraf3 = document.createElement('p'); 
-
-    paragraf3.textContent = element.paragraf;
-
-    goals.appendChild(paragraf3);
-
-})
-
-    var textHistory = document.querySelector(".history-text");
-    var textGoals = document.querySelector(".goals-text");
-    var textVision = document.querySelector(".vision-text");
-
-
+paragraf2.style.display = 'none';
+paragraf3.style.display = 'none';
 
 btnVision.addEventListener("click", function(){
+    if (btnVision.textContent =='Bigdrop' ){
 
-
-    textVision.classList.remove("hidden");
-    textHistory.classList.add("hidden");
-    textGoals.classList.add("hidden");
-
+        paragraf.style.display = 'none';
+        paragraf2.style.display = 'block';
+        paragraf3.style.display = 'none';
+       
+    }
 })
 
 btnHistory.addEventListener("click", function(){
+    if (btnHistory.textContent =='Tommy' ){
 
-
-    textHistory.classList.remove("hidden");
-    textVision.classList.add("hidden");
-    textGoals.classList.add("hidden");
-
+        paragraf2.style.display = 'none';
+        paragraf.style.display = 'block';
+        paragraf3.style.display = 'none';
+       
+    } 
 })
 
 btnGoals.addEventListener("click", function(){
+    if (btnGoals.textContent =='Cuker' ){
+
+        paragraf.style.display = 'none';
+        paragraf2.style.display = 'none';
+        paragraf3.style.display = 'block';
+       
+    }
+})
+
+})
 
 
-    textVision.classList.add("hidden");
-    textGoals.classList.remove("hidden");
-    textHistory.classList.add("hidden");
 
-    })
+    
+    // var vision = document.createElement('div');
+    
+    // vision.classList.add('vision-text');
+
+    // vision.classList.add('hidden');
+
+    // tabContents.appendChild(vision);
+
+
+    // const paragraf2 = document.createElement('p'); 
+
+    // paragraf2.textContent = element.paragraf;
+
+    // vision.appendChild(paragraf2);
+
+
+    // var goals = document.createElement('div');
+
+    // goals.classList.add('goals-text');
+    
+    // goals.classList.add('hidden');
+    
+    // tabContents.appendChild(goals);
+
+
+    // const paragraf3 = document.createElement('p'); 
+
+    // paragraf3.textContent = element.paragraf;
+
+    // goals.appendChild(paragraf3);
+
+
+//     var textHistory = document.querySelector(".history-text");
+//     var textGoals = document.querySelector(".goals-text");
+//     var textVision = document.querySelector(".vision-text");
+
+
+
+// btnVision.addEventListener("click", function(){
+
+
+//     textVision.classList.remove("hidden");
+//     textHistory.classList.add("hidden");
+//     textGoals.classList.add("hidden");
+
+// })
+
+// btnHistory.addEventListener("click", function(){
+
+
+//     textHistory.classList.remove("hidden");
+//     textVision.classList.add("hidden");
+//     textGoals.classList.add("hidden");
+
+// })
+
+// btnGoals.addEventListener("click", function(){
+
+
+//     textVision.classList.add("hidden");
+//     textGoals.classList.remove("hidden");
+//     textHistory.classList.add("hidden");
+
+//     })
